d2c54bfdb7bc610f78b3bd4d67d914d4941273ca
DimaPolishuk1995 <dima.polishuk.1995@gmail.com>
2023-03-11 19:00:16 +0200

    create store and global style

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {Provider} from 'react-redux';
+
+import store from './store';
 import App from './App';
+import GlobalStyle from './styles/GlobalStyle';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <GlobalStyle />
+            <App/>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
