76df42f1a3750f50c4602c6080f653c33bf47356
DimaPolishuk1995 <dima.polishuk.1995@gmail.com>
2023-03-13 19:26:47 +0200

    update index and App component

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import {Provider} from 'react-redux';
 
 import store from './store';
 import App from './App';
-import GlobalStyle from './styles/GlobalStyle';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <GlobalStyle />
             <App/>
         </Provider>
     </React.StrictMode>
