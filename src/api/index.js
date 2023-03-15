955bbfcdeb84708924fe91740534c20d1036702e
DimaPolishuk1995 <dima.polishuk.1995@gmail.com>
2023-03-15 20:59:41 +0200

    update GITHUB_TOKEN

--- src/api/index.js
+++ src/api/index.js
@@ -2,13 +2,9 @@ import axios from 'axios';
 
 const GITHUB_API_BASE_URL = 'https://api.github.com';
 
-const token = process.env.REACT_APP_GITHUB_TOKEN;
-const headers = {};
-if (token) {
-    headers.Authorization = `Bearer ${token}`;
-}
-
 export const instance = axios.create({
     baseURL: GITHUB_API_BASE_URL,
-    headers,
+    headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    },
 });
